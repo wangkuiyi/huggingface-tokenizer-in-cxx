47fe112de951064d17bb71e2743d28abc9d91eef
Yi Wang <yi.wang.2005@gmail.com>
2023-02-11 22:27:41 -0800

    Clean up code

--- tool/data.py
+++ tool/data.py
@@ -7,21 +7,20 @@
 #
 import pandas as pd
 
-artists = pd.read_csv('/tmp/artists-data.csv') # 4168 artists
-artists = artists[artists['Popularity']>5] # 297 popular artists
+artists = pd.read_csv("/tmp/artists-data.csv")  # 4168 artists
+artists = artists[artists["Popularity"] > 5]  # 297 popular artists
 
-lyrics = pd.read_csv('/tmp/lyrics-data.csv') # 379931 songs
-lyrics = lyrics[lyrics['language']=='fr']
+lyrics = pd.read_csv("/tmp/lyrics-data.csv")  # 379931 songs
+lyrics = lyrics[lyrics["language"] == "fr"]
 
-df = lyrics.merge(artists[['Artist', 'Genres', 'Link']],
-                  left_on='ALink', right_on='Link', how='inner')
+df = lyrics.merge(
+    artists[["Artist", "Genres", "Link"]], left_on="ALink", right_on="Link", how="inner"
+)
 
-# df = df.drop(columns=['ALink','SLink','language','Link'])
+df = df[df["Lyric"].apply(lambda x: len(x.split(" ")) < 350)]
 
-df = df[df['Lyric'].apply(lambda x: len(x.split(' ')) < 350)]
-
-df = df['Lyric'] # 317 songs
+df = df["Lyric"]  # 317 songs
 
 for row in df:
     # https://stackoverflow.com/a/2077944/724872
-    print(' '.join(row.replace('\n', ' ').split()))
+    print(" ".join(row.replace("\n", " ").split()))
