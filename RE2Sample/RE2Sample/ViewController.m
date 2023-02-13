ca37a6ad80bea2a3a7c03399a6849af677b30d10
Yi Wang <yi.wang.2005@gmail.com>
2023-02-12 19:55:31 -0800

    Build RE2 into XCFramework and test it (#15)
    * Separate building of re2 and bpe
    
    * Add bulid-re2.sh
    
    * Add RE2Sample iOS app
    
    * ObjC call C++ call RE2

---
+++ RE2Sample/RE2Sample/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  RE2Sample
+//
+//  Created by Yi Wang on 2/12/23.
+//
+
+#import "ViewController.h"
+
+void hello();
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    hello();
+}
+
+
+@end
