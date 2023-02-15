c3c31cc07fa0ca07573e66f323f51d0b3c92dc75
Yi Wang <yi.wang.2005@gmail.com>
2023-02-15 11:42:26 -0800

    Update pytorch-gpt-generate.py

--- tool/pytorch-gpt-generate.py
+++ tool/pytorch-gpt-generate.py
@@ -11,14 +11,20 @@ model = GPT2LMHeadModel.from_pretrained("gpt2")
 
 prompt_text = "Hello, my dog is cute"
 input = tokenizer.encode(prompt_text)
+# mask = [0] * 4 + [1] * len(input)
+# input = [0] * 4 + input
+mask = [1] * len(input)
+
 input = torch.tensor([input], dtype=torch.int32)
-mask = torch.ones(input.shape, dtype=torch.int32)
+mask = torch.tensor([mask], dtype=torch.float32)
 for _ in range(10):
+    print(input)
+    print(mask)
     outputs = model(input_ids=input, attention_mask=mask)
     next_token_logits = outputs.logits[:, -1]
     val, idx = torch.topk(next_token_logits, 1)
     input = torch.cat([input, idx], axis=1)
-    mask = torch.cat([mask, torch.tensor([[1]], dtype=torch.int32)], dim=1)
+    mask = torch.cat([mask, torch.tensor([[1]], dtype=torch.float32)], dim=1)
 text = tokenizer.decode(input[0].tolist())
 print(text)
 
